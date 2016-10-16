cf30db7e920d980af5004b7e6dd6b1d8f34c5d4b
HugoDelgado <hugojdm@gmail.com>
2016-10-16 11:45:38 -0400

    Add files via upload

---
+++ Socrata2Sql.aspx
@@ -0,0 +1,139 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Socrata2Sql.aspx.cs" Inherits="Socrata2SqlMigrationTool.Socrata2Sql" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Socrata 2 SQL Migration Tool</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <img src="logo2.jpg" runat="server" />
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" runat="server" href="~/Socrata2Sql.aspx">Socrata2SqlServer Migration Tool</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" >
+            
+            <h3>Soctrata to SQL Server Migration Tool - JSON</h3>
+            <img src="logo1.jpg" runat="server"/>
+            <asp:Label ID="LabelTBGeturl" runat="server" Text="Please write the url of the resource:"></asp:Label>
+            <asp:TextBox ID="TBurlSocrata" runat="server"></asp:TextBox>
+            <asp:Button ID="ButtonGetUrl" runat="server" Text="Analyze Table" OnClick="GetUrl_Click" CssClass="bg-warning" />
+            <asp:Button ID="ButtonInsert" runat="server" Text="Insert Data" CssClass="bg-warning" OnClick="InsertData_Click" Visible="false"/>
+            <asp:LinkButton ID="ButtonCancel" runat="server" CssClass="bg-warning" Visible="false" Text="Cancel" PostBackUrl="~/Socrata2Sql.aspx"></asp:LinkButton>   
+
+        </div>
+
+        <div class="container">
+            <asp:Label ID="LabelUId" runat="server" Text="Unique Identifier:"></asp:Label>
+            <asp:Label ID="LabelGotUid" runat="server"></asp:Label>
+        </div>
+        <div class="container">
+            <asp:Table runat="server" Width="100%" CssClass="table">
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:Label ID="LabelFormat" runat="server" Text="Format:"></asp:Label></asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label ID="LabelGotFormat" runat="server"></asp:Label></asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label ID="LabelNumRows" runat="server" Text="Number of Rows:"></asp:Label></asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label ID="LabelGotNumRows" runat="server" Text=""></asp:Label></asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label ID="LabelNumFields" runat="server" Text="Number of fields:"></asp:Label></asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label ID="LabelGotNumRecords" runat="server" Text=""></asp:Label></asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </div>
+        <div class="container">
+            <asp:Label ID="LabelContent" runat="server" Text=""></asp:Label> 
+        </div>
+
+        <div class="container">
+            <asp:Label ID="LabelData" runat="server" Text=""></asp:Label> 
+        </div>
+        <div class="container">
+            <asp:GridView ID="GVDataTypes" runat="server" AutoGenerateColumns="False" DataSourceID="SQLgetStructure" BackColor="LightGoldenrodYellow" 
+                BorderColor="Tan" BorderWidth="1px" CellPadding="2" ForeColor="Black" GridLines="None" Font-Size="Small">
+                <AlternatingRowStyle BackColor="PaleGoldenrod" />
+                <Columns>
+                    <asp:BoundField DataField="COLUMN_NAME" HeaderText="COLUMN_NAME" SortExpression="COLUMN_NAME"></asp:BoundField>
+                    <asp:TemplateField HeaderText="DATA_TYPE" SortExpression="DATA_TYPE">
+                        <EditItemTemplate>
+                            <asp:DropDownList ID="DropDownList1" runat="server" >
+                                <asp:ListItem Text="Varchar" Value="VARCHAR"></asp:ListItem>
+                                <asp:ListItem Text="Integer 32 " Value="INT"></asp:ListItem>
+                                <asp:ListItem Text="Integer 64" Value="LONG"></asp:ListItem>
+                                <asp:ListItem Text="Datetime" Value="DATETIME"></asp:ListItem>
+                            </asp:DropDownList>
+                            <asp:Label ID="Label1" runat="server" Text='<%# Eval("DATA_TYPE") %>'></asp:Label>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="Label1" runat="server" Text='<%# Bind("DATA_TYPE") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="CHARACTER_MAXIMUM_LENGTH" HeaderText="CHARACTER_MAXIMUM_LENGTH" ReadOnly="True" SortExpression="CHARACTER_MAXIMUM_LENGTH"></asp:BoundField>
+                    <asp:BoundField DataField="NUMERIC_PRECISION" HeaderText="NUMERIC_PRECISION" ReadOnly="True" SortExpression="NUMERIC_PRECISION"></asp:BoundField>
+                    <asp:BoundField DataField="DATETIME_PRECISION" HeaderText="DATETIME_PRECISION" ReadOnly="True" SortExpression="DATETIME_PRECISION"></asp:BoundField>
+                    <asp:BoundField DataField="IS_NULLABLE" HeaderText="IS_NULLABLE" ReadOnly="True" SortExpression="IS_NULLABLE"></asp:BoundField>
+                    <asp:CommandField ShowEditButton="True" />
+                </Columns>
+                <FooterStyle BackColor="Tan" />
+                <HeaderStyle BackColor="Tan" Font-Bold="True" />
+                <PagerStyle BackColor="PaleGoldenrod" ForeColor="DarkSlateBlue" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="DarkSlateBlue" ForeColor="GhostWhite" />
+                <SortedAscendingCellStyle BackColor="#FAFAE7" />
+                <SortedAscendingHeaderStyle BackColor="#DAC09E" />
+                <SortedDescendingCellStyle BackColor="#E1DB9C" />
+                <SortedDescendingHeaderStyle BackColor="#C2A47B" />
+            </asp:GridView>
+            <asp:SqlDataSource runat="server" ID="SQLgetStructure" ConnectionString='<%$ ConnectionStrings:Socrata2SqlConnectionString %>' SelectCommand="select COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, 
+       NUMERIC_PRECISION, DATETIME_PRECISION, 
+       IS_NULLABLE 
+from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME=@tablename">
+                <SelectParameters>
+                    <asp:ControlParameter ControlID="LabelGotUid" PropertyName="Text" DefaultValue="" Name="tablename"></asp:ControlParameter>
+                </SelectParameters>
+            </asp:SqlDataSource>
+          
+        </div>
+
+        <div class="container">
+            <asp:GridView ID="GVTableUploaded" runat="server" DataSourceID="SqlTableUploaded" Visible="False" BackColor="LightGoldenrodYellow" BorderColor="Tan" BorderWidth="1px" CellPadding="2" ForeColor="Black" GridLines="None" Font-Size="Smaller">
+                <AlternatingRowStyle BackColor="PaleGoldenrod" />
+                <FooterStyle BackColor="Tan" />
+                <HeaderStyle BackColor="Tan" Font-Bold="True" />
+                <PagerStyle BackColor="PaleGoldenrod" ForeColor="DarkSlateBlue" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="DarkSlateBlue" ForeColor="GhostWhite" />
+                <SortedAscendingCellStyle BackColor="#FAFAE7" />
+                <SortedAscendingHeaderStyle BackColor="#DAC09E" />
+                <SortedDescendingCellStyle BackColor="#E1DB9C" />
+                <SortedDescendingHeaderStyle BackColor="#C2A47B" />
+            </asp:GridView>
+            <asp:SqlDataSource runat="server" ID="SqlTableUploaded" ConnectionString='<%$ ConnectionStrings:Socrata2SqlConnectionString %>' SelectCommand="">
+            </asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Socrata2Sql.aspx.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web.UI;
+
+namespace Socrata2SqlMigrationTool
+{
+    public partial class Socrata2Sql : Page
+    {
+        //  Conection string a la base de datos sql
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Socrata2SqlConnectionString"].ConnectionString;
+        // variable del url del que se halara la data
+        string url;
+        // nombre de la base de datos que se creara o usara en sql
+        string databaseName = "SocrataFiles";
+        //string que retiene el row sobre el que se está trabajando
+        private string rowstring;
+        //hala el contenido del archivo y lo coloca en textfromfile
+        WebClient wc = new WebClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //crea el query para crear la base de datos la primera vez
+            //string dbquery = GetDbCreationQuery(databaseName);
+            //cada vez que inicie la pagina creara o verificara si la base de datos existe
+            //ExecuteQuery(dbquery);
+
+        }
+
+        //metodo que repsonde al presionar el boton luego de entrar el url
+        protected void GetUrl_Click(object sender, EventArgs e)
+        {
+            ButtonInsert.Visible = true;
+            ButtonGetUrl.Visible = false;
+            ButtonCancel.Visible = true;
+            //hala el url del textbox
+            url = TBurlSocrata.Text;
+            //llama el metodo getuniqueidentifier del cual hala el uid del url 
+            //coloca ese nombre en el texto del label got uid           
+            string tablename = GetUniqueIdentifier(url);
+            LabelGotUid.Text = tablename;
+            //crea un string que contiene el formato del archivo
+            string format = GetFormat(url);
+            //coloca el string en el label
+            LabelGotFormat.Text = format;
+
+            
+            wc.Headers.Add(HttpRequestHeader.AcceptCharset, "UTF-8");
+            var textFromFile = wc.DownloadString(url);
+            textFromFile = Regex.Replace(textFromFile, @"\r\n?|\n","");
+            //cuenta cuantos rows hay usando el corchete de cierre
+            int numberofrows = CountStringOnString(textFromFile, "}");
+            //cuenta cuantos records tiene el row mas grande...
+            // se debe modificar para anadir rows. Considerar subir la data transacional y hacer pivots
+            int numberofrecords = FillRowString(textFromFile,numberofrows);
+            //tira el contenido del textfromfile a la pagina       
+
+            LabelGotNumRows.Text = numberofrows.ToString();
+            LabelGotNumRecords.Text = numberofrecords.ToString();
+
+            string tableQuery = CreateTableQuery(rowstring, numberofrecords, tablename);
+            ExecuteQuery(tableQuery);
+            //string tableQuery = GenerateColumnList(rowstring, numberofrecords);
+            LabelContent.Text = "Table "+tablename+" was succesfully added to database"+ databaseName;
+
+            
+        }
+        string CreateRowstring(string textFromFile)
+        {
+            textFromFile = GetRemainingString(textFromFile, "{");
+            return GetRow(textFromFile);
+        }
+        string GetDataForTable(string textFromFile, int numberofrows, string tablename)
+        {
+            int rowpointer = 0;
+            string query = "";
+            while (rowpointer < numberofrows)
+            {
+                query += "INSERT INTO [" + tablename + "] " ;
+                string columname = " (";
+                string data = " VALUES (";
+                string currentrow = GetRow(textFromFile);
+                int numberofrecords = CountStringOnString(currentrow, ",");
+                int recordpointer = 0;
+                string record;
+                string currentrecord;
+                string currentcolumn;
+                while (recordpointer < numberofrecords)
+                {
+                    currentrecord = GetRecord(currentrow);
+                    currentcolumn = GetColumnName(currentrecord).Trim();
+                    columname += "["+ currentcolumn + "], ";
+                    record = GetDatafromRecord(currentrecord);
+                    
+                    if (IsDate(currentcolumn))
+                    { record = "\'" + record.Replace('T', ' ').Trim() + "\'"; }
+                    data += record + ", ";
+                    currentrow = GetRemainingString(currentrow, ",");
+                    recordpointer++;
+                }
+                currentrecord = GetRecord(currentrow);
+                currentcolumn = GetColumnName(currentrecord).Trim();
+                columname += "[" +currentcolumn + "]) ";
+                record = GetDatafromRecord(currentrecord);
+                if (IsDate(currentcolumn))
+                { record = "\'"  +record.Replace('T', ' ').Trim() + "\'"; }
+                data += record + "); ";
+                //incorporar codigo que verifique presencia de columnas y 
+                //anada columnas que faltan con alter table
+                query += columname + data;
+                textFromFile = GetRemainingString(textFromFile, "}");
+                rowpointer++;
+            }
+            ExecuteQuery(query);
+            return "Data has been inserted succefully " ;
+        }
+        string GetDatafromRecord(string record)
+        {
+            record = GetRemainingString(record, ":");
+            return record.Replace("\"","");
+        }
+        private int FillRowString(string textFromFile, int numberofrows)
+        {
+            rowstring = CreateRowstring(textFromFile);
+            //cuenta cuantos campos hay en el primer row
+            int numberofrecords = CountStringOnString(rowstring, ",");
+
+            int rowpointer = 0;
+            while (rowpointer< numberofrows)
+            {
+                string newrow = CreateRowstring(textFromFile);
+                int newNumberOfRecords = CountStringOnString(newrow, ",");
+                if (newNumberOfRecords > numberofrecords)
+                {
+                  rowstring = newrow;
+                  numberofrecords = CountStringOnString(rowstring, ",");
+                }
+                rowpointer++;
+            }
+            return numberofrecords;
+        }
+        //metodo que hala el unique identifier del url
+        private string GetUniqueIdentifier(string url)
+        {
+            //detecta la localizacion del index de la instancia de la palabra reosurce
+            int filenameIndex = url.IndexOf("resource/");
+            //pica el string y solo hala el pedazo que corresponde al UID
+            if (url != "")
+            {
+                string filename = url.Substring(filenameIndex + 9);
+                //identifica la localizacion del punto
+                int formatIndex = filename.IndexOf(".");
+                //pica el string para dar solamente el unique identifier
+                filename = filename.Substring(0, formatIndex);
+                //devuelve el filename
+                return filename;
+            }
+            else
+            {
+                Response.Redirect(Request.RawUrl);
+                return "";
+            }
+            
+        }
+
+        //hala solo el formato del string
+        private string GetFormat(string url)
+        {
+            //busca la primera instancia de la palabra resource
+            int filenameIndex = url.IndexOf("resource/");
+            string filename = url.Substring(filenameIndex + 9);
+            //pica el filename
+            int formatIndex = filename.IndexOf(".");
+            //pica el formato
+            string format = filename.Substring(formatIndex + 1);
+            //devuelve el formato
+            return format;
+        }
+
+        //metodo que crea el query para crear bases de datos
+        static string GetDbCreationQuery(string dbName)
+        {
+            // db creation query
+            string query = "IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'"+dbName+"') BEGIN";
+            query += " CREATE DATABASE "+ dbName +" END; USING " + dbName;
+            return query;
+        }
+
+        //metodo que verifica si la base de datos existe
+        public static bool CheckDatabaseExists(string connectionString, string databaseName)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand(string.Format(
+                       "SELECT db_id('{0}')", databaseName), connection))
+                {
+                    connection.Open();
+                    return (command.ExecuteScalar() != DBNull.Value);
+                }
+            }
+        }
+
+        //funcion que cuenta cantidad de instancias de un string en otro
+        private int CountStringOnString(string haystack, string needle)
+        {
+            int count = 0, n = 0;
+
+            if (needle != "")
+            {
+                while ((n = haystack.IndexOf(needle, n)) != -1)
+                {
+                    n += needle.Length;
+                    ++count;
+                }
+            }
+            return count;
+        }
+        //funcion que obtiene el row
+        string GetRow(string fulltext)
+        {
+            if (fulltext.IndexOf("{") >= 0)
+            { fulltext = fulltext.Substring(fulltext.IndexOf("{") + 1); }
+            if (fulltext.IndexOf("}") >= 0)
+            { fulltext = fulltext.Remove(fulltext.IndexOf("}")); }
+            return fulltext;
+        }
+
+        //funcion que reduce el string eliminando el row analizado
+        string GetRemainingString(string fulltext, string cutstring)
+        {
+            return fulltext.Substring(fulltext.IndexOf(cutstring) + 1);
+        }
+        string GetRecord(string fulltext)
+        {
+            if (fulltext.IndexOf(",") > 0)
+            { return fulltext.Substring(1, fulltext.IndexOf(",")-1); }
+            else
+            { return fulltext; }
+
+        }
+        string GetColumnName(string record)
+        {
+            record = record.Trim();
+            if (record.IndexOf(":") > 0)
+            { record = record.Substring(1, record.IndexOf(":")-3); }
+            //record = Regex.Replace(record, "[^A-Za-z0-9 _]", "");
+            record = record.Replace(" ", "_");
+            //limite maximo del nombre de las columnas
+            if (record.Length > 127)
+            { record = record.Substring(0, 127); }
+            return record;
+        }
+
+        string GenerateColumnList(string rowstring, int numberofrecords)
+        {
+            int rowPointer = 0;
+            string rowlist = "";
+            string currentrecord; string currentdatatype; string currentcolumn;
+
+            while (rowPointer < numberofrecords)
+            {
+                currentrecord = GetRecord(rowstring);
+                currentcolumn = GetColumnName(currentrecord);
+                currentdatatype = GetDataType(GetDatafromRecord(currentrecord).Trim(), currentcolumn);
+                rowstring = GetRemainingString(rowstring, ",");
+                rowlist = rowlist + " [" + currentcolumn + "] " + currentdatatype + ",";
+                rowPointer++;
+            }
+            currentrecord = GetRecord(rowstring);
+            currentcolumn = GetColumnName(currentrecord);
+            currentdatatype = GetDataType(GetDatafromRecord(currentrecord).Trim(), currentcolumn);
+            return rowlist + " [" + currentcolumn + "] " + currentdatatype;
+        }
+
+        string GetDataType(string data, string columname)
+        {
+            if (IsDate(columname))
+            {
+                return "datetime";
+            }
+            else if (IsOnlyNumbers(data))
+            {
+                return "int";
+            }
+            //los data type ints y decimal se tienen que evaluar con
+            //herramientas del SQL usando un cast() por que hay que mirar todos
+            //los records para luego decidr el tipo de dato que corresponde a todos
+            // los datos de dicha columna (pueden ser todos enteros excepto uno ya es decimal)
+            else
+            {
+                return "VARCHAR(MAX)";
+            }
+        }
+
+        public static bool IsOnlyNumbers(string data)
+        {
+            Regex regex = new Regex("^[0-9]+$");
+
+            if (regex.IsMatch(data))
+            {
+                return true;
+            }
+            else { return false; }
+            //return strValidateString.All(char.IsDigit);
+        }
+
+        public static bool Isdecimal(string str)
+        {
+            int first = str.IndexOf(".");
+            string noperiod = str.Replace(".","");
+            if (((first >= 0) && (first - str.LastIndexOf(".")) == 0) &&
+                noperiod.All(char.IsDigit))
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public static bool IsDate(string columname)
+        {
+            //establecer criterios para idetificar columnas basados en el nombre de la columna
+            if ((columname.ToLower() == "fecha")||
+                (columname.ToLower() == "date") ||
+                (columname.ToLower() == "mes")||
+                (columname.ToLower() == "month"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string CreateTableQuery(string rowstring, int numberofrecords, string tablename)
+        {
+            string tableQuery = "IF(EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES " +
+                                 " WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = '" + tablename + "')) BEGIN " + 
+                                 " DROP TABLE [" + tablename + "] END;";
+            tableQuery += " CREATE TABLE [" + tablename + "](";
+            tableQuery += GenerateColumnList(rowstring, numberofrecords);
+            return tableQuery + ");";
+        }
+
+        //metodo que ejecuta un query en forma de string
+        void ExecuteQuery(string query)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand command = new SqlCommand(query, con);
+            command.ExecuteNonQuery();
+
+        }
+
+        protected void InsertData_Click(object sender, EventArgs e)
+        {
+            url = TBurlSocrata.Text;
+            var textFromFile = wc.DownloadString(url);
+            textFromFile = Regex.Replace(textFromFile, @"\r\n?|\n", "");
+            //cuenta cuantos rows hay usando el corchete de cierre
+            int numberofrows = CountStringOnString(textFromFile, "}");
+            LabelData.Text = GetDataForTable(textFromFile, numberofrows, LabelGotUid.Text);
+            GVDataTypes.Visible = false;
+            GVTableUploaded.Visible = true;
+            ButtonInsert.Visible = false;
+            ButtonGetUrl.Visible = false;
+            ButtonCancel.Visible = true;
+            ButtonCancel.Text = "Back";
+            SqlTableUploaded.SelectCommand = "Select * from [" + LabelGotUid.Text +"]";
+            Page.DataBind();
+        }
+    }
+}
